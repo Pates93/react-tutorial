a5d1faf5d80eff4a2e866d8f5bc7fb25896901d8
Pates93 <patur093@hotmail.com>
2019-02-01 13:02:53 +0000

    Updates

---
+++ precache-manifest.94678aba2adcf52a5a2f821ec8153e21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "20086fa20df91eb6bf4d",
+    "url": "/react-tutorial/static/js/main.20086fa2.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "20086fa20df91eb6bf4d",
+    "url": "/react-tutorial/static/css/main.a0aba394.chunk.css"
+  },
+  {
+    "revision": "fc97db2ad5cab246e87f5899b72b5592",
+    "url": "/react-tutorial/index.html"
+  }
+];
